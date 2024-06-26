48322b8e6ca20c5457a1dc7aafbf2ba739aab7a8
Gamer2012 <65019724+YetAnotherCoder00@users.noreply.github.com>
2024-06-27 00:26:09 +0200

    did a lot of stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,137 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+    const jsonTodos = localStorage.getItem("todos") || "";
+    let parsed: any;
+
+    if (jsonTodos !== "") {
+        parsed = JSON.parse(jsonTodos);
+    }
+    else {
+        parsed = [];
+    }
+
+    let todoDiv: HTMLDivElement;
+
+    let id: number = 0;
+    let title: string = "";
+    let description: string = "";
+    let important: boolean = false;
+    let urgent: boolean = false;
+    let category: number = 0;
+    let author: string = "";
+    let start: Date;
+    let end: Date;
+    let completed: boolean = false;
+
+    try {
+        id = parsed[parsed.length - 1]["id"] + 1;
+    }
+    catch {
+        id = 0;
+    }
+
+    async function createTodo(event: SubmitEvent) {
+        event.preventDefault();
+
+        const todo = {
+            "id": id,
+            "title": title,
+            "description": description,
+            "important": important,
+            "urgent": urgent,
+            "category": category,
+            "author": author,
+            "start": start,
+            "end": end,
+            "completed": completed
+        }
+
+        id += 1;
+
+        parsed = [...parsed, todo]; // reassignment of the variable causes the html to rerender that component
+        
+        const result: string = JSON.stringify(parsed);
+
+        localStorage.setItem("todos", result);
+        // todoDiv.hidden = true;
+
+        console.log(parsed);
+    }
+
+    function newTodo() {
+        todoDiv.hidden = false;
+    }
+    
+    async function quickEdit(event: SubmitEvent) {
+        event.preventDefault();
+    }
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Todos</title>
+	<meta name="description" content="Todo App for IT" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h1>Todo list</h1>
+    <div class="todos">
+        {#each parsed as todo}
+        <form on:submit={quickEdit}>
+            <input type="checkbox" bind:checked={todo.completed} disabled />
+            {todo.title}
+            <button type="submit">submit</button>
+        </form>
+        {/each}
+    </div>
+    <button on:click={newTodo}>new</button>
+    <div bind:this={todoDiv} hidden>
+        <button on:click={() => {todoDiv.hidden = true}}>close</button>
+        <form on:submit={createTodo}>
+            <input type="hidden" bind:value={id} />
+            <p>
+                <label for="title">title:</label>
+                <input type="text" name="title" bind:value={title} required />
+            </p>
+            <p>
+                <label for="description">description:</label>
+                <input type="text" name="description" bind:value={description} required />
+            </p>
+            <p>
+                <label for="important">important:</label>
+                <input type="checkbox" name="important" bind:checked={important} />
+            </p>
+            <p>
+                <label for="urgent">urgent:</label>
+                <input type="checkbox" name="urgent" bind:checked={urgent} />
+            </p>
+            <p>
+                <label for="category">category:</label>
+                <input type="number" name="category" bind:value={category} required />
+            </p>
+            <p>
+                <label for="author">author:</label>
+                <input type="text" name="author" bind:value={author} required />
+            </p>
+            <p>
+                <label for="start">start:</label>
+                <input type="date" name="start" bind:value={start} required />
+            </p>
+            <p>
+                <label for="end">end:</label>
+                <input type="date" name="end" bind:value={end} required />
+            </p>
+            <p>
+                <label for="completed">completed:</label>
+                <input type="checkbox" name="completed" bind:checked={completed}/>
+            </p>
+            <button type="submit">submit</button>
+        </form>
+    </div>
 </section>
 
 <style>
 	section {
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
 		align-items: center;
 		flex: 0.6;
 	}
@@ -41,19 +140,32 @@
 		width: 100%;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+    form {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        gap: 0.5vh;
+    }
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+    p {
+        gap: 0.5vh;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        font-size: 2vh;
+        margin: 0;
+        justify-content: space-between;
+    }
+
+    label {
+        text-align: right;
+    }
+
+    input {
+        display: block;
+        align-self: center;
+        height: 2vh;
+        border: none;
+        padding: 0.5vh;
+    }
 </style>
